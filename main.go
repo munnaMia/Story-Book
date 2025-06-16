e837c701a2ee85868e7d90ec423a3c40f62bdf09
Md Munna Mia <mdmunnamia987@gmail.com>
2025-06-16 20:59:40 +0600

    add: refc: http post method check in create route

--- main.go
+++ main.go
@@ -6,8 +6,15 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	/*
+		r.URL.Path != "/"
+		=================
+
+		"/" - this route work as a wildcard which give response to any route that user hit
+		to prevent this happen we use this line.
+	*/
 	if r.URL.Path != "/" {
-		http.NotFound(w,r)
+		http.NotFound(w, r)
 		return
 	}
 	w.Write([]byte("Welcome to Storybook"))
@@ -18,6 +25,33 @@ func blogView(w http.ResponseWriter, r *http.Request) {
 }
 
 func blogCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		/*
+			r.Method != "POST"
+			==================
+
+			This block of code check the method and return a 405 status code with a massage
+			"Method Not Allowed" using w.WriteHeader(http.StatusMethodNotAllowed) & w.write("Method not allowed")
+			
+			Learn more about 
+				- https://pkg.go.dev/net/http#pkg-constants
+				- https://pkg.go.dev/net/http#DetectContentType
+
+
+			!) It’s only possible to call w.WriteHeader() once per response, and after the
+			status code has been written it can’t be changed. If you try to call w.WriteHeader()
+			a second time Go will log a warning message.
+			
+			1-- first sample:
+			-----------------
+				w.Header().Set("Allow", "POST")
+				w.WriteHeader(http.StatusMethodNotAllowed)
+				w.Write([]byte("Method Not Allowed"))
+		*/
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
 	w.Write([]byte("Create a new blog..."))
 }
 
